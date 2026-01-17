ea1652b50a33d6f3dd3a3b52c230265366b58978
Anup Chauhan <84135396+anupchauhanak6@users.noreply.github.com>
2026-01-17 13:13:43 +0530

    Wrap application in AuthProvider for user authentication context

--- client/main-app/src/pages/_app.js
+++ client/main-app/src/pages/_app.js
@@ -1,15 +1,18 @@
 import "@/globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { AuthProvider } from "../context/AuthContext";
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="flex-1">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <AuthProvider>
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="flex-1">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 }
